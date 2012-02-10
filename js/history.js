11d9dadeed9a4d2f0685219470ea98121a9b25c4
mn <mn@mn-xr.(none)>
2012-02-11 02:31:34 +0600

    base history operations

--- js/history.js
+++ js/history.js
@@ -1,58 +1,43 @@
-var History = function(canvas) {
+/*
+ * объект сохранения истории
+ * */
+var History = function(canvas, options) {
     var it = this;
 
-    $('#back').click(function() {
-        $(document).trigger('story:back');
-        return false;
-    })
-    $('#forward').click(function() {
-        $(document).trigger('story:forward');
-        return false;
-    })
+    this.opt = jQuery.extend({
+        back: '#back',
+        forward: '#forward',
+    }, options);
+
+    this.setButtonsState();
+
+    $(document).on('click', this.opt.back,    function() { it.backHistory();    return false });
+    $(document).on('click', this.opt.forward, function() { it.forwardHistory(); return false });
 
     this.canvas = canvas;
+
     var _selected = {}
     $(document).on('story:add', function(e, data) {
-        var set_selected = function() {
-            _selected = {};
-            _selected['can_el'] = data.can_el;
-            _selected['left']   = data.can_el.getLeft();
-            _selected['top']    = data.can_el.getTop();
-            _selected['width']  = data.can_el.getWidth();
-            _selected['height'] = data.can_el.getHeight();
-            _selected['scaleX'] = data.can_el.get('scaleX');
-            _selected['scaleY'] = data.can_el.get('scaleY');
-            _selected['angle']  = data.can_el.getAngle();
-            _selected['flipX']  = data.can_el.get('flipX')
-            _selected['flipY']  = data.can_el.get('flipY')
-            //_selected['']     =
-        }
-        if(data.type == 'object:selected') {
-            set_selected()
-        }
+        if(data.type == 'object:selected') { _selected = it.setState(data) };
         if(data.type == 'object:modified') {
             if (_selected.can_el != data.can_el) {
                 console.log('History - объекты не равны', _selected.can_el, data.can_el);
             } else {
                 History.back.push(_selected);
-                set_selected(data);
+                History.forward = [];
+                _selected = it.setState(data);
             }
+            it.setButtonsState();
         }
     });
-
-    $(document).on('story:back', function(e, data) {
-        it.backHistory();
-    });
-    $(document).on('story:forward', function(e, data) {
-        it.forwardHistory();
-    });
 }
-History.back = [];
+
+History.back = []; 
 History.forward = [];
 
+// Восстанавливает состояние объекта полученное из истории
 History.prototype.restoreObjectState = function(obj) {
     if (obj) {
-        console.log(obj);
         obj.can_el.set({
             left: obj.left, top: obj.top,
             scaleX: obj.scaleX, scaleY: obj.scaleY,
@@ -64,13 +49,54 @@ History.prototype.restoreObjectState = function(obj) {
 }
 History.prototype.backHistory = function() {
     var back = History.back.pop();
-    this.restoreObjectState(back);
-    History.forward.push(back)
+    if (back) {
+        History.forward.push(this.setState(back));
+        this.restoreObjectState(back);
+    }
+    this.setButtonsState();
 }
 History.prototype.forwardHistory = function() {
     var forward = History.forward.pop();
-    this.restoreObjectState(forward);
-    History.back.push(forward);
+    if (forward) {
+        History.back.push(this.setState(forward));
+        this.restoreObjectState(forward);
+    }
+    this.setButtonsState();
+}
+
+// запоминает состояние объекта
+History.prototype.setState = function(data) {
+    var state = {};
+    state['can_el'] = data.can_el;
+    state['left']   = data.can_el.getLeft();
+    state['top']    = data.can_el.getTop();
+    state['width']  = data.can_el.getWidth();
+    state['height'] = data.can_el.getHeight();
+    state['scaleX'] = data.can_el.get('scaleX');
+    state['scaleY'] = data.can_el.get('scaleY');
+    state['angle']  = data.can_el.getAngle();
+    state['flipX']  = data.can_el.get('flipX')
+    state['flipY']  = data.can_el.get('flipY')
+    //state['']     =
+    return state;
+}
+
+History.prototype.setButtonsState = function() {
+    if (!History.back.length) {
+        $(this.opt.back).css('opacity', '.5');
+        $(this.opt.back).css('cursor', 'default');
+
+    } else {
+        $(this.opt.back).css('opacity', '1');
+        $(this.opt.back).css('cursor', 'pointer');
+    }
+    if (!History.forward.length) {
+        $(this.opt.forward).css('opacity', '.5');
+        $(this.opt.forward).css('cursor', 'default');
+    } else {
+        $(this.opt.forward).css('opacity', '1');
+        $(this.opt.forward).css('cursor', 'pointer');
+    }
 }
 
 //h='';// it for debug
